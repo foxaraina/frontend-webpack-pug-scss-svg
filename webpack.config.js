cdaaf7249e9de34b6d41c3953e4c678337190fc2
Stasy <dovzhik.a@gmail.com>
2020-11-26 14:21:54 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,177 @@
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+const PAGES_DIR = path.resolve(__dirname, 'src/views/pages');
+const PAGES = fs.readdirSync(PAGES_DIR).filter((fileName) => fileName.endsWith('.pug'));
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: './scripts/index.js',
+  },
+  output: {
+    filename: 'scripts/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    historyApiFallback: true,
+    compress: true,
+    port: 4040,
+    noInfo: true,
+    quiet: true,
+    clientLogLevel: 'warning',
+    stats: 'errors-only',
+    open: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src/data/data-map.json'),
+          to: path.resolve(__dirname, 'dist/data'),
+        },
+      ],
+    }),
+    ...PAGES.map((page) => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/, '.html')}`,
+      minify: false,
+    })),
+    new CleanWebpackPlugin(),
+    new SpriteLoaderPlugin({ plainSprite: true }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new ImageminWebpWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      "jQuery": "jquery",
+      "window.jQuery": "jquery",
+      "jquery": "jquery",
+      "window.jquery": "jquery",
+      "$": "jquery",
+      "window.$": "jquery"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+              publicPath: '../../',
+
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|webp|svg)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        }],
+      },
+      {
+        type: 'javascript/auto',
+        test: /\.json$/,
+        include: [path.resolve(__dirname, 'src/data')],
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        exclude: /svgIcons/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        }],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
+      {
+        test: /\.svg$/,
+        include: path.resolve(__dirname, 'src/img/svgIcons'),
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        include: path.resolve(__dirname, 'src/img/svg'),
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+            options: {
+              extract: true,
+            },
+          },
+          'svgo-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+        ],
+      },
+    ],
+  },
+};
